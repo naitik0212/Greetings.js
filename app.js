bf14f02d59fce0311048d104f6538a98d6038726
naitik0212 <shahnaitik0212@gmail.com>
2019-02-24 17:22:00 -0700

    developing landing page

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-var g = G$('Naitik', 'Shah');
-g.greetUser().setLanguage('en').greetUser(true).log();
-console.log(g);
+// var g = G$('Naitik', 'Shah');
+// g.greetUser().setLanguage('en').greetUser(true).log();
+// console.log(g);
 
-$('#login').click(function () {
-    var loginGreetr = G$('Naitik', 'Shah');
+
+
+
+function Submit() {
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var language = $('#lang').val();
+    // console.log(firstName);
+    // console.log(lastName)
+    // console.log(language);
+
+    var loginGreetr = G$(firstName, lastName, language);
 
     $('#loginForm').hide();
 
     loginGreetr.setLanguage($('#lang').val()).HTMLGreeting('#greetings').log();
 
-});
+
+
+}
